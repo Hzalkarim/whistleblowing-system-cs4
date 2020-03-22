947dcff89927f68bb501d042dc68003ae62e172a
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-22 14:58:41 +0700

    add basic templates

---
+++ page/admin.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col-12">
+        <div class="jumbotron my-3">
+            <h1 class="display-3">Tampilan Admin Dari Req</h1>
+        </div>
+    </div>
+    
+</div>
