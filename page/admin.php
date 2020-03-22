80063739782c757c34e5e4b3e5690d7df5e68d54
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-22 15:21:08 +0700

    Added dummy data

--- page/admin.php
+++ page/admin.php
@@ -1,8 +1,40 @@
+<?php
+    $data = Array(
+        "Hafizh;2 Maret 2020;Saya Sendiri;Tidur dikelas;Karena mengantuk;klik link ini",
+        "Anomim;2 Maret 2020;Saya Sendiri;Tidur dikelas;Karena mengantuk;klik link ini",
+        "Rizky;2 Maret 2020;Saya Sendiri;Tidur dikelas;Karena mengantuk;klik link ini",
+        "Aqilla;2 Maret 2020;Saya Sendiri;Tidur dikelas;Karena mengantuk;klik link ini"
+    );
+?>
+
+
 <div class="row">
     <div class="col-12">
         <div class="jumbotron my-3">
-            <h1 class="display-3">Tampilan Admin Dari Req</h1>
+            <h3 class="display-4">Tampilan Data Pelaporan</h1>
         </div>
     </div>
-    
+    <div class="col-12">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Pelapor</th>
+                    <th>Tanggal Pelaporan</th>
+                    <th>Pelanggar</th>
+                    <th>Pelanggaran</th>
+                    <th>Penjelasan</th>
+                    <th>Bukti</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach($data as $str):?>
+                <tr>
+                <?php foreach(explode(';', $str) as $d):?>
+                    <td><?php echo $d?></td>
+                <?php endforeach?>
+                </tr>
+            <?php endforeach?>
+            </tbody>
+        </table>
+    </div>
 </div>
