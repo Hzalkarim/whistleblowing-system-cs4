5a60ee083703d240addecd65d9601383ddf829e9
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-22 22:45:29 +0700

    Minor change test

--- page/home.php
+++ page/home.php
@@ -2,7 +2,7 @@
   <div class="col-12">
     <div class="jumbotron text-center">
       <h1 class="display-1">Whistleblowing System</h1>
-      <h2 class="mt-4">Jumlah Laporan: 5000</h2>
+      <h2 class="mt-4">Jumlah Laporan: 7000</h2>
     </div>
   </div>
 </div>
