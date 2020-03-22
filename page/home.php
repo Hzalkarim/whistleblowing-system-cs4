980e37f5e0a75f130fc5eb611f355cf9d64902af
H_Alkarim <hafizh.alkarim@gmail.com>
2020-03-22 18:06:15 +0700

    initial commit

---
+++ page/home.php
@@ -0,0 +1,8 @@
+<div class="row">
+  <div class="col-12">
+    <div class="jumbotron text-center">
+      <h1 class="display-1">Whistleblowing System</h1>
+      <h2 class="mt-4">Jumlah Laporan: 5000</h2>
+    </div>
+  </div>
+</div>
