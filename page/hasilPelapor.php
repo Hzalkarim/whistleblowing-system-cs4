042ec949f661e34fdd6305fa18817040ca78dce8
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-22 22:35:46 +0700

    Minor change

--- page/hasilPelapor.php
+++ page/hasilPelapor.php
@@ -1,51 +1,53 @@
-<div class="container">
-	<h1>Riwayat Pelaporan</h1>
-	<hr />
-	<table class="table table-striped">
-    	<thead>
-      	<tr>
-        	<th>Pelapor</th>
-        	<th>Tanggal Pelaporan</th>
-        	<th>Pelanggar</th>
-        	<th>Kategori</th>
-        	<th>Pengaduan</th>
-        	<th>Bukti</th>
-      	</tr>
-    	</thead>
-    	<tbody>
-      	<tr>
-        	<td>karim</td>
-        	<td><?php
-			date_default_timezone_set("Asia/Jakarta");
-			echo date("Y/m/d");
-			?></td>
-        	<td>Rizky</td>
-        	<td>Layanan Kedisiplinan</td>
-        	<td>Ketiduran di kelas karakter</td>
-        	<td><a href="#">Klik di sini</a></td>
-      	</tr>
-      	<tr>
-        	<td>karim</td>
-        	<td><?php
-			date_default_timezone_set("Asia/Jakarta");
-			echo date("Y/m/d");
-			?></td>
-        	<td>Rizky</td>
-        	<td>Layanan Kedisiplinan</td>
-        	<td>Ketiduran di kelas karakter</td>
-        	<td><a href="#">Klik di sini</a></td>
-      	</tr>
-      	<tr>
-        	<td>karim</td>
-        	<td><?php
-			date_default_timezone_set("Asia/Jakarta");
-			echo date("Y/m/d");
-			?></td>
-        	<td>Rizky</td>
-        	<td>Layanan Kedisiplinan</td>
-        	<td>Ketiduran di kelas karakter</td>
-        	<td><a href="#">Klik di sini</a></td>
-      	</tr>
-    	</tbody>
-  	</table>
+<div class="row">
+	<div class="col-12">
+		<h1>Riwayat Pelaporan</h1>
+		<hr />
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th>Pelapor</th>
+					<th>Tanggal Pelaporan</th>
+					<th>Pelanggar</th>
+					<th>Kategori</th>
+					<th>Pengaduan</th>
+					<th>Bukti</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>karim</td>
+					<td><?php
+					date_default_timezone_set("Asia/Jakarta");
+					echo date("Y/m/d");
+					?></td>
+					<td>Rizky</td>
+					<td>Layanan Kedisiplinan</td>
+					<td>Ketiduran di kelas karakter</td>
+					<td><a href="#">Klik di sini</a></td>
+				</tr>
+				<tr>
+					<td>karim</td>
+					<td><?php
+					// date_default_timezone_set("Asia/Jakarta");
+					echo date("Y/m/d");
+					?></td>
+					<td>Rizky</td>
+					<td>Layanan Kedisiplinan</td>
+					<td>Ketiduran di kelas karakter</td>
+					<td><a href="#">Klik di sini</a></td>
+				</tr>
+				<tr>
+					<td>karim</td>
+					<td><?php
+					// date_default_timezone_set("Asia/Jakarta");
+					echo date("Y/m/d");
+					?></td>
+					<td>Rizky</td>
+					<td>Layanan Kedisiplinan</td>
+					<td>Ketiduran di kelas karakter</td>
+					<td><a href="#">Klik di sini</a></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
