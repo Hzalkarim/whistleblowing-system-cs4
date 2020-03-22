34387256d7a207845dee834a70a7ee86e9b85363
H_Alkarim <hafizh.alkarim@gmail.com>
2020-03-22 17:38:18 +0700

    Added component

---
+++ page/component/footer.php
@@ -0,0 +1,7 @@
+<footer class="bg-light">
+    <div class="container">
+        <div class="row">
+            <h4 class="col-12 mt-2 text-right">Footer</h4>
+        </div>
+    </div>
+</footer>
---
+++ page/component/header.php
@@ -0,0 +1,22 @@
+<header class="bg-light">
+    <div class="container">
+        <div class="row">
+            <h4 class="col-12 mt-2 text-left">Heder</h4>
+            <div class="col-12">
+                <nav class="navbar navbar-expand navbar-light">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php?view=home">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?view=pelapor">Pelapor View</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?view=admin">Admin View</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
