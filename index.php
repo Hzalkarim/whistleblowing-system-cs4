b67c815b89a38677d82a7b563c397ca8835ea7bc
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-22 14:57:49 +0700

    add dynamic page setup

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin View</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/wbstyle.css">
+</head>
+<body>
+
+<header class="bg-light">
+    <div class="container">
+        <div class="row">
+            <h4 class="col-12 mt-2 text-left">Heder</h4>
+            <div class="col-12">
+                <nav class="navbar navbar-expand navbar-light">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="index.php?view=home">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?view=pelapor">Pelapor View</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php?view=admin">Admin View</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+
+<main>
+    <div class="container my-2">
+        <?php 
+        
+        if (!isset($_GET['view'])) $_GET['view'] = 'home';
+        switch ($_GET['view']){
+            case 'home':
+                echo "<h3>Selamat Datang</h3>";
+                break;
+            case 'pelapor':
+                require "page/pelapor.php";
+                break;
+            case 'admin':
+                require "page/admin.php";
+                break;
+            default:
+                echo "404 Not Found";
+        }
+        
+        ?>
+    </div>
+</main>
+
+<footer class="bg-light">
+    <div class="container">
+        <div class="row">
+            <h4 class="col-12 mt-2 text-right">Footer</h4>
+        </div>
+    </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
