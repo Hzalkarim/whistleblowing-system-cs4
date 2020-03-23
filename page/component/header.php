475e0f4d4420dbd571136e8560071dd482e280a5
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-23 23:49:27 +0700

    Added animating sidebar

--- page/component/header.php
+++ page/component/header.php
@@ -13,6 +13,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="index.php?view=admin">Admin View</a>
                         </li>
+                        <li class="nav-item">
+                            <a id="side-toggle" class="nav-link" href="#">Sidebar</a>
+                        </li>
                     </ul>
                     <ul class="nav navbar-nav navbar-right">
                     <?php if (!isset($_POST['user'])):?>
