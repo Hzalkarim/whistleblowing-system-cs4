f9881bb9b5bed94b1c90d9ebcf8e3f18695c6c28
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-24 22:36:31 +0700

    Minor change

--- page/home.php
+++ page/home.php
@@ -1,9 +1,10 @@
 <div class="row">
-  <div class="col-12">
-    <div class="text-center">
-      <h1 class="display-1">Whistleblowing System</h1>
-      <h2 class="my-4">Jumlah Laporan: 7000</h2>
-      <img src="asset/img/flow.svg" alt="Flow">
+    <div class="col-12 text-center bg-light mt-0">
+        <h1 class="display-1 home-title p-3">Whistleblowing System</h1>
+    </div>
+    <div class="col-12 text-center">
+        <h2 class="my-4">Jumlah Laporan: 7000</h2>
+        <img src="asset/img/flow.svg" id="flow" alt="Flow">
     </div>
   </div>
 </div>
