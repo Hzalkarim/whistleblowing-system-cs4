5182e6602d8690c79703a019eb04ad77103e5a90
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-24 18:37:44 +0700

    Added dummy flowchart

--- page/home.php
+++ page/home.php
@@ -1,8 +1,9 @@
 <div class="row">
   <div class="col-12">
-    <div class="jumbotron text-center">
+    <div class="text-center">
       <h1 class="display-1">Whistleblowing System</h1>
-      <h2 class="mt-4">Jumlah Laporan: 7000</h2>
+      <h2 class="my-4">Jumlah Laporan: 7000</h2>
+      <img src="asset/img/flow.svg" alt="Flow">
     </div>
   </div>
 </div>
