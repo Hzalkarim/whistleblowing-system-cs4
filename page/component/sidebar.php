7a919f49dc4286a7c60328d366297b106f248032
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-24 18:11:17 +0700

    Added logo

--- page/component/sidebar.php
+++ page/component/sidebar.php
@@ -1,4 +1,6 @@
-<aside class="bg-dark">
-    <p class="text-white mt-5">Halo, Dunia</p>
+<aside class="bg-dark p-5">
+    <center>
+        <img src="asset/img/Apaya.svg" class="logo" alt="Logo">
+    </center>
     <button id="side-toggle" class="btn btn-dark"></button>
 </aside>
