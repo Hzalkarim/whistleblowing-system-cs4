833d98083d32afe2ae27e84681397450dc43ec1e
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-26 21:19:39 +0700

    Minor change

--- page/home.php
+++ page/home.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="col-12 text-center bg-light mt-0">
-        <h1 class="display-1 home-title p-3">Whistleblowing System</h1>
+        <h1 class="display-1 home-title">Whistleblowing System</h1>
     </div>
     <div class="col-12 text-center">
         <h2 class="my-4">Jumlah Laporan: 7000</h2>
