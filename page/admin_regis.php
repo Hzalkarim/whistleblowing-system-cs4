bfbfa0c681aecfaa17358a5ca391e85ae922ff2f
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-26 23:04:39 +0700

    Initial commit

---
+++ page/admin_regis.php
@@ -0,0 +1,46 @@
+<div class="row">
+    <div class="col"></div>
+    <div class="col-sm-12 col-md-8 col-lg-6 bg-light mt-5 rounded">
+        <div class="text-center my-4">
+            <h4>Form Pendaftaran Akun Administrator</h4>
+        </div>
+        <form class="mt-4">
+            <div class="form-group">
+                <label for="nama-lengkap">Nama Lengkap</label>
+                <input type="email" class="form-control" placeholder="Masukkan Nama">
+            </div>
+            <div class="form-group">
+                <label for="alamat-email">Alamat Email</label>
+                <input type="email" class="form-control" placeholder="Masukkan Email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group my-3">
+                <label class="mr-5" for="jenis-kel">Jenis Kelamin</label>
+                <div class="form-check-inline">
+                    <label for="pria">
+                        <input type="radio" name="jk" value="l"> Laki - Laki
+                    </label>
+                </div>
+                <div class="form-check-inline">
+                    <label for="wanita">
+                        <input type="radio" name="jk" value="p"> Perempuan
+                    </label>
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="alamat">Alamat Tinggal</label>
+                <textarea name="name" class="form-control" rows="8" cols="80"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="no-telp">Nomor Telepon</label>
+                <input type="number" class="form-control" placeholder="Masukkan no Telepon">
+            </div>
+            <button type="submit" class="btn btn-primary mb-4">Registrasi</button>
+            <button type="reset" class="btn btn-danger mb-4">Hapus</button>
+        </form>
+    </div>
+    <div class="col"></div>
+</div>
