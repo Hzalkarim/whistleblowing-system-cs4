ee3cf4d1a8a12684617948c03e0f63a5fef5a5d3
hzalkarim <hafizh.alkarim02@gmail.com>
2020-03-26 21:20:25 +0700

    Restructuring dynamic page

--- index.php
+++ index.php
@@ -1,33 +1,40 @@
 <?php
 
-$page_title = "Sistem Pengaduan Dari Kami";
-require "page/component/header.php"
+$pages = Array(
+    'home' => 'Sistem Pengaduan Kampus',
+    'pelapor' => 'Submit Pengaduan',
+    'hasilPelapor' => 'Histori Pengaduan',
+    'admin' => 'Daftar Pengaduan'
+);
+
+if (!isset($_GET['view'])) $_GET['view'] = 'home';
+
+$page_title = '';
+$page_exist = false;
+if (array_key_exists($_GET['view'], $pages)){
+    $page_title = $pages[$_GET['view']];
+    $page_exist = true;
+} else {
+    $page_title = "Page not found";
+}
+
+require "page/component/header.php";
+
 ?>
 
-<main>
-    <div class="container">
-        <?php
-
-        if (!isset($_GET['view'])) $_GET['view'] = 'home';
-        switch ($_GET['view']){
-            case 'home':
-                require "page/home.php";
+<main class="container">
+    <?php
+    if (!$page_exist){
+        echo '<div class="display-1">404 Not Found</div>';
+    } else {
+        foreach ($pages as $page => $p_title) {
+            if ($_GET['view'] == $page){
+                require 'page/' . $page . '.php';
                 break;
-            case 'pelapor':
-                require "page/pelapor.php";
-                break;
-            case 'hasilPelapor':
-                require "page/hasilPelapor.php";
-                break;
-            case 'admin':
-                require "page/admin.php";
-                break;
-            default:
-                echo '<div class="display-1">404 Not Found</div>';
+            }
         }
-
-        ?>
-    </div>
+    }
+    ?>
 </main>
 
 <?php require "page/component/footer.php" ?>
