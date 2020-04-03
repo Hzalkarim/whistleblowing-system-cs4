2fb5da643fca5cfa6005e71bd5f7c6d4eb7ac2a2
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-03 22:17:30 +0700

    Added name attribute

--- page/mhs_regis.php
+++ page/mhs_regis.php
@@ -7,14 +7,14 @@
         <form class="mt-4">
             <div class="form-group">
                 <label for="nama-lengkap">Nama Lengkap</label>
-                <input type="email" class="form-control" placeholder="Masukkan Nama">
+                <input type="email" name="nama" class="form-control" placeholder="Masukkan Nama">
             </div>
             <div class="form-group">
                 <label for="nama-lengkap">NIM</label>
-                <input type="email" class="form-control" placeholder="Masukkan NIM">
+                <input type="email" name="nim" class="form-control" placeholder="Masukkan NIM">
             </div>
             <div class="form-group">
-                <label for="nama-lengkap">Program Studi</label>
+                <label for="prodi">Program Studi</label>
                 <select class="form-control" name="prodi">
                     <option value="bm">Bisnis Manajemen</option>
                     <option value="si">Sistem Informasi</option>
@@ -23,11 +23,11 @@
             </div>
             <div class="form-group">
                 <label for="alamat-email">Alamat Email</label>
-                <input type="email" class="form-control" placeholder="Masukkan Email">
+                <input type="email" name="email" class="form-control" placeholder="Masukkan Email">
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" class="form-control" placeholder="Password">
+                <input type="password" name="password" class="form-control" placeholder="Password">
             </div>
             <div class="form-group my-3">
                 <label class="mr-5" for="jenis-kel">Jenis Kelamin</label>
@@ -44,11 +44,11 @@
             </div>
             <div class="form-group">
                 <label for="alamat">Alamat Tinggal</label>
-                <textarea name="name" class="form-control" rows="8" cols="80"></textarea>
+                <textarea name="alamat" class="form-control" rows="8" cols="80"></textarea>
             </div>
             <div class="form-group">
                 <label for="no-telp">Nomor Telepon</label>
-                <input type="number" class="form-control" placeholder="Masukkan no Telepon">
+                <input type="number" name="no-telp" class="form-control" placeholder="Masukkan no Telepon">
             </div>
             <button type="submit" class="btn btn-primary mb-4">Registrasi</button>
             <button type="reset" class="btn btn-danger mb-4">Hapus</button>
