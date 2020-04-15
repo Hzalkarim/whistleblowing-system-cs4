e8f1ed4ad62896e84f575a39ac360f0be88409dd
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-15 23:18:25 +0700

    Added new properties

--- class/model/User.php
+++ class/model/User.php
@@ -1,61 +1,92 @@
 <?php
-	class User extends WbModel{
-		private $user_id;
-		private $email;
-		private $password;
-		private $nama;
-		private $role;
 
+class User extends WbModel{
+	
+	private $user_id;
+	private $email;
+	private $password;
+	private $role;
+	private $nama;
+	private $gender;
+	private $alamat;
+	private $no_telp;
 
-		function setUserId($user_id){
-			$this->user_id = $user_id;
-		}
+	protected function setColumnFunc() {
+		$colfun = Array(
+			"id" => "UserId",
+			"email" => "Email",
+			"password" => "Password",
+			"role" => "Role",
+			"nama" => "Nama",
+			"gender" => "Gender",
+			"alamat" => "Alamat",
+			"no_telp" => "NoTelp"
+		);
 
-		function getUserId(){
-			return $this->user_id;
-		}
-
-		function setEmail($email){
-			$this->email = $email;
-		}
-
-		function getEmail(){
-			return $this->email;
-		}
-
-		function setNama($nama){
-			$this->nama = $nama;
-		}
-
-		function getNama(){
-			return $this->nama;
-		}
-
-		function setPassword($password){
-			$this->password = $password;
-		}
-
-		function getPassword(){
-			return $this->password;
-		}
-
-		function setRole($role){
-			$this->role = $role;
-		}
-
-		function getRole(){
-			return $this->role;
-		}
-
-		protected function setColumnFunc() {
-			$colfun = Array(
-				"user_id" => "UserId",
-				"email" => "Email",
-				"pwd" => "Password",
-				"nama" => "Nama"
-			);
-
-			$this->columnFunc = $colfun;
-		}
+		$this->columnFunc = $colfun;
 	}
-?>
+
+	function setUserId($user_id){
+		$this->user_id = $user_id;
+	}
+
+	function getUserId(){
+		return $this->user_id;
+	}
+
+	function setEmail($email){
+		$this->email = $email;
+	}
+
+	function getEmail(){
+		return $this->email;
+	}
+
+	function setNama($nama){
+		$this->nama = $nama;
+	}
+
+	function getNama(){
+		return $this->nama;
+	}
+
+	function setPassword($password){
+		$this->password = $password;
+	}
+
+	function getPassword(){
+		return $this->password;
+	}
+
+	function setRole($role){
+		$this->role = $role;
+	}
+
+	function getRole(){
+		return $this->role;
+	}
+
+	public function getGender(){
+	    return $this->gender;
+	}
+
+    public function setGender($gender) {
+	    $this->gender = $gender;
+	}
+
+  	public function getAlamat(){
+	    return $this->alamat;
+	}
+
+    public function setAlamat($alamat) {
+	    $this->alamat = $alamat;
+	}
+
+  	public function getNoTelp(){
+	    return $this->no_telp;
+	}
+
+    public function setNoTelp($no_telp) {
+	    $this->no_telp = $no_telp;
+	}
+}
