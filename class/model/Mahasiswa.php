f8f75f9c6882de648ffc6baa6c26c2f768193710
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-15 23:21:03 +0700

    Fixed properties according to database

--- class/model/Mahasiswa.php
+++ class/model/Mahasiswa.php
@@ -1,58 +1,41 @@
 <?php
-	class Mahasiswa{
-		private $nama;
-		private $nim;
-		private $no_telp;
-		private $alamat;
-		private $gender;
-		private $prodi;
+class Mahasiswa extends WbModel{
+	private $nim;
+	private $user_id;
+	private $prodi;
 
-		function setNama($nama){
-			$this->nama = $nama;
-		}
+	protected function setColumnFunc(){
+		$colfun = Array(
+			"nim" => "Nim",
+			"user_id" => "UserId",
+			"kode_prodi" => "Prodi"
+		);
 
-		function getNama(){
-			return $this->nama; 
-		}
-
-		function setNim($nim){
-			$this->nim = $nim;
-		}
-
-		function getNim(){
-			return $this->nim;
-		}
-
-		function setNoTelp($no_telp){
-			$this->no_telp = $no_telp;
-		}
-
-		function getNoTelp(){
-			return $this->no_telp;
-		}
-
-		function setAlamat($alamat){
-			$this->alamat = $alamat;
-		}
-
-		function getAlamat(){
-			return $this->alamat;
-		}
-
-		function setGender($gender){
-			$this->gender = $gender;
-		}
-
-		function getGender(){
-			return $this->gender;
-		}
-
-		function setProdi($prodi){
-			$this->prodi = $prodi;
-		}
-
-		function getProdi(){
-			return $this->prodi;
-		}
+		$this->columnFunc = $colfun;
 	}
-?>
+
+	public function getNim(){
+	    return $this->nim;
+	}
+
+	    public function setNim($nim) {
+	    $this->nim = $nim;
+	}
+
+	public function getUserId(){
+	    return $this->user_id;
+	}
+
+	public function setUserId($user_id) {
+	    $this->user_id = $user_id;
+	}
+
+	public function getProdi(){
+	    return $this->prodi;
+	}
+
+	public function setProdi($prodi) {
+	    $this->prodi = $prodi;
+	}
+
+}
