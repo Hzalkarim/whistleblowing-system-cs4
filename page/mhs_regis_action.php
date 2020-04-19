360c70100828560a2efb6e13520995f9c883478f
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-19 14:17:34 +0700

    Added random Id generator

--- page/mhs_regis_action.php
+++ page/mhs_regis_action.php
@@ -3,22 +3,46 @@ require_once "../class/wb_model.php";
 require_once "../class/wb_controller.php";
 require_once "../class/model/User.php";
 require_once "../class/controller/UserController.php";
-// require_once "class/model/Mahasiswa.php";
-// require_once "class/controller/MahasiswaController.php";
+require_once "../class/model/Mahasiswa.php";
+require_once "../class/controller/MahasiswaController.php";
 
 $user = new User();
 $user->setAllValues($_POST);
 $user->setPassword(md5($_POST['password']));
-$user->setUserId($_POST['nama'] . "-ID");
 $user->setRole("Mahasiswa");
 
 $userCt = new UserController();
-if ($userCt->insert($user)){
+$i = 0;
+do {
+    $user->setUserId(rand(1, 10000));
+} while (!$userCt->insert($user) && ++$i < 3);
+
+$userEmail = new User();
+$userEmail->setEmail($user->getEmail());
+
+$user = $userCt->where($userEmail)->select();
+
+if (!is_null($user)) {
+    $mhs = new Mahasiswa();
+    $mhs->setAllValues($_POST);
+    $mhs->setUserId($user->getUserId());
+
+    $mhsCt = new MahasiswaController();
+    $mhsCt->insert($mhs);
+
     setcookie('user_nama', $user->getNama(), 0, "/");
     setcookie('user_id', $user->getUserId(), 0, "/");
 } else {
     echo '<script>alert("Gagal")</script>';
 }
+
+
+// if ($userCt->insert($user)){
+//     setcookie('user_nama', $user->getNama(), 0, "/");
+//     setcookie('user_id', $user->getUserId(), 0, "/");
+// } else {
+//     echo '<script>alert("Gagal")</script>';
+// }
 
 header("Location: ../index.php");
 
