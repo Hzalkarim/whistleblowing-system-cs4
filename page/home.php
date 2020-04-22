794aa02d99f55021ed2eae27866fe93d5c36f4c2
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-22 20:52:58 +0700

    Added success message

--- page/home.php
+++ page/home.php
@@ -6,6 +6,19 @@ $total = $pengaduanCt->getCount();
 ?>
 
 <div class="row">
+<?php if (isset($_COOKIE['submit_berhasil'])): ?>
+    <div class="col-12">
+        <div class="alert alert-success">
+            <b>Success!</b> Pengaduan berhasil disubmit.
+        </div>
+    </div>
+<?php elseif (isset($_COOKIE['submit_gagal'])): ?>
+    <div class="col-12">
+        <div class="alert alert-danger">
+            <b>Danger!</b> Pengaduan gagal disubmit.
+        </div>
+    </div>
+<?php endif; ?>
     <div class="jumbotron col-12 text-center bg-light mt-0">
         <h1 class="home-title">Whistleblowing System</h1>
     </div>
