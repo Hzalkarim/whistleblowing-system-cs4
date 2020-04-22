4997ab32af88bb332cf7d9c783a5262069761a7c
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-22 11:11:56 +0700

    Fetching kategori from database

--- page/pelapor.php
+++ page/pelapor.php
@@ -1,3 +1,16 @@
+<?php
+
+require_once "class/model/Kategori.php";
+require_once "class/controller/KategoriController.php";
+
+$kategoriCt = new KategoriController();
+$listKategori = $kategoriCt->select();
+//
+// echo "<pre>";
+// print_r($listKategori[0]->getValues());die;
+
+?>
+
 <div class="row">
     <div class="col-12">
         <h1>Form Pengaduan</h1>
@@ -10,32 +23,26 @@
                     <!-- <input type="text" class="form-control" placeholder="Pelapor" id="pelapor" name="pelapor"> -->
                     <div class="form-check-inline">
                         <label for="user">
-                            <input type="radio" name="pelapor" value="user" checked> Sebagai Mahasiswa
+                            <input type="radio" name="pelapor" value="Sebagai Mahasiswa" checked> Sebagai Mahasiswa
                         </label>
                     </div>
                     <div class="form-check-inline">
                         <label for="anon">
-                            <input type="radio" name="pelapor" value="anon"> Sebagai Anonim
+                            <input type="radio" name="pelapor" value="Anonim"> Sebagai Anonim
                         </label>
                     </div>
                 </div>
-                <!-- <div class="form-group">
-                    <label for="pelanggar">Pihak Terkait :</label>
-                    <input type="text" class="form-control" placeholder="Pelanggar" id="pelanggar" name="pihak-terkait">
-                </div> -->
                 <div class="form-group">
-                    <label for="sel1">Kategori Pengaduan:</label>
-                    <select class="form-control" id="sel1">
-                        <option>Layanan Kedisiplinan</option>
-                        <option>Layanan Akedemik</option>
-                        <option>Layanan Sarana Prasarana</option>
-                        <option>Layanan Keuangan</option>
-                        <option>Layanan Perpustakaan</option>
+                    <label for="id_kategori">Kategori Pengaduan:</label>
+                    <select class="form-control" name="id_kategori">
+                    <?php foreach($listKategori as $kategori):?>
+                        <option value="<?php echo $kategori->getId() ?>"><?php echo $kategori->getNama() ?></option>
+                    <?php endforeach; ?>
                     </select>
                 </div>
                 <div class="form-group">
                     <label for="pelapor">Judul :</label>
-                    <input type="text" class="form-control" placeholder="Pelapor" id="pelapor" name="judul">
+                    <input type="text" class="form-control" placeholder="Judul" name="judul">
                 </div>
                 <div class="form-group">
                     <label for="pengaduan">Pengaduan :</label>
@@ -45,7 +52,7 @@
                     <label for="bukti">Bukti :</label>
                     <input type="file" class="form-control-file border" name="bukti">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary" name="btnSubmit">Submit</button>
             </form>
         </div>
     </div>
