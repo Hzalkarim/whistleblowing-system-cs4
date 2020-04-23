6afa91dfbc6935b6aca223cb8a773aa35e231ff8
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-23 23:09:03 +0700

    Extended WbModel

--- class/model/PenindakLanjut.php
+++ class/model/PenindakLanjut.php
@@ -1,58 +1,58 @@
 <?php
-	class PenindakLanjut{
-		private $nama;
-		private $id_pegawai;
-		private $alamat;
-		private $no_telp;
-		private $gender;
-		private $bidang;
+class PenindakLanjut extends WbModel{
+	private $id_pegawai;
+	private $user_id;
+	private $bidang;
 
-		function setNama($nama){
-			$this->nama = $nama;
-		}
-
-		function getNama(){
-			return $this->nama;
-		}
-
-		function setIdPegawai($id_pegawai){
-			$this->id_pegawai = $id_pegawai;
-		}
-
-		function getIdPegawai(){
-			return $this->id_pegawai;
-		}
-
-		function setAlamat($alamat){
-			$this->alamat = $alamat;
-		}
-
-		function getAlamat(){
-			return $this->alamat;
-		}
-
-		function setNoTelp($no_telp){
-			$this->no_telp = $no_telp;
-		}
-
-		function getNoTelp(){
-			return $this->no_telp;
-		}
-
-		function setGender($gender){
-			$this->gender = $gender;
-		}
-
-		function getGender(){
-			return $this->gender;
-		}
-
-		function setBidang($bidang){
-			$this->bidang = $bidang;
-		}
-
-		function getBidang(){
-			return $this->bidang;
-		}
+	public function getPrimaryKey(){
+		return $this->getIdPegawai();
 	}
-?>
+
+    protected function setColumnFunc(){
+		$colfun = Array(
+			'id_pegawai' => 'IdPegawai',
+			'user_id' => 'UserId',
+			'bidang' => 'Bidang'
+		);
+
+		$this->columnFunc = $colfun;
+	}
+
+    protected function setTableName(){
+		$this->tableName = "penindak_lanjut";
+	}
+
+    protected function setForeignKeys(){
+		$fk = Array(
+			'user' => 'user_id'
+		);
+
+		$this->foreignKeys = $fk;
+	}
+
+
+	public function getIdPegawai(){
+		return $this->id_pegawai;
+	}
+
+	public function setIdPegawai($id_pegawai) {
+		$this->id_pegawai = $id_pegawai;
+	}
+
+	public function getUserId(){
+		return $this->user_id;
+	}
+
+	public function setUserId($user_id) {
+		$this->user_id = $user_id;
+	}
+
+	public function getBidang(){
+		return $this->bidang;
+	}
+
+	public function setBidang($bidang) {
+		$this->bidang = $bidang;
+	}
+
+}
