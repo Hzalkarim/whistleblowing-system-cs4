b55385f3aaeb045b44528b3d04142d7fd02109fe
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-25 01:36:04 +0700

    Print to-be-sent link

--- page/admin_daftarkan.php
+++ page/admin_daftarkan.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="col-lg-4 col-md-2"></div>
     <div class="col-lg-4 col-md-8 col-sm-12 text-center bg-light rounded p-4">
-        <form method="post" action="login.php">
+        <form method="post" action="index.php?view=admin_daftarkan">
             <div class="form-group">
                 <label for="user">Email:</label>
                 <input type="text" class="form-control" name="email">
@@ -15,3 +15,14 @@
     </div>
     <div class="col-lg-4 col-md-2"></div>
 </div>
+
+<?php
+
+if (isset($_POST['btn-submit']) && $_POST['email'] != '' && $_POST['password'] != ''){
+    $getParam = "&p=" . md5($_POST['password']) . "&e=" . md5($_POST['email']);
+    $link = "index.php?view=pegawai_regis{$getParam}";
+
+    echo $link;
+}
+
+?>
