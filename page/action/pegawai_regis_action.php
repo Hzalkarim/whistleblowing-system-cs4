919dbe9f9cacb5d8a81cdcccaad6c00c5acb9f98
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-25 01:24:58 +0700

    Minor fix

--- page/action/pegawai_regis_action.php
+++ page/action/pegawai_regis_action.php
@@ -6,23 +6,31 @@ if (isset($_POST['btn-submit'])) {
     require_once "../../class/wb_model.php";
     require_once "../../class/model/User.php";
     require_once "../../class/controller/UserController.php";
+    require_once "../../class/model/PenindakLanjut.php";
+    require_once "../../class/controller/PenindakLanjutController.php";
 
     $user = new User();
     $user->setEmail($_POST['email']);
 
     $userCt = new UserController();
     $findUser = $userCt->where($user)->selectOne();
-    echo "<pre>";
-    print_r($findUser);
 
     $findUser->setAllValues($_POST);
     $findUser->setPassword(md5($_POST['password']));
-    print_r($findUser->getAllValues());
 
     $user->setEmail(NULL);
     $user->setId($findUser->getId());
 
     $userCt->update($findUser);
+
+    if ($findUser->getRole() == "Penindak Lanjut"){
+        $pLanjut = new PenindakLanjut();
+        $pLanjut->setAllValues($_POST);
+        $pLanjut->setUserId($user->getId());
+
+        $pLanjutCt = new PenindakLanjutController();
+        $pLanjutCt->insert($pLanjut);
+    }
 
     header("Location: ../../index.php?view=home");
 }
