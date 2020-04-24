4e7c730f64c6227f5998ed90858c1cfd42534cfb
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-24 19:52:46 +0700

    Added new dynamic pages

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ $pages = Array(
     'pegawai' => 'Daftar Pengaduan',
     'pegawai_regis' => 'Registrasi Pegawai',
     'mhs_regis' => 'Registrasi Mahasiswa',
-    'admin_pengaduan' => 'Administrator - List Pengaduan'
+    'admin_pengaduan' => 'Administrator - List Pengaduan',
+    'admin_penugasan' => 'Administrator - Penugasan'
 );
 
 if (!isset($_GET['view'])) $_GET['view'] = 'home';
