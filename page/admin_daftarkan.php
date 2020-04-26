e33c96c3757c0c0f54e78a9fd9012f5b62edf8a9
hzalkarim <hafizh.alkarim02@gmail.com>
2020-04-26 10:36:17 +0700

    Implemented insert user and result box

--- page/admin_daftarkan.php
+++ page/admin_daftarkan.php
@@ -1,28 +1,60 @@
+<?php
+$result = false;
+if (isset($_POST['btn-submit']) && $_POST['email'] != '' && $_POST['password'] != ''){
+    require_once "class/controller/UserController.php";
+    require_once "class/model/User.php";
+
+    $user = new User();
+    $user->setEmail($_POST['email']);
+    $user->setPassword(md5($_POST['password']));
+
+    $userCt = new UserController();
+    $result = $userCt->insert($user);
+}
+
+if ($result):
+
+$getParam = "&p=" . md5($_POST['password']) . "&e=" . md5($_POST['email']);
+$link = "?view=pegawai_regis{$getParam}";
+
+?>
+<div class="row">
+    <div class="col-xs-0 col-md-2"></div>
+    <div class="col-xs-12 col-md-8">
+        <div class="alert alert-success" style="overflow-x: auto">
+            <b>Berhasil!</b> Pendaftaran user baru berhasil.<br><br>
+            Silakan share link berikut: <a href="<?php echo 'index.php'.$link ?>">
+                <i><?php echo $_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'].$link ?></i><br><br>
+            </a>
+            Atau beri tahu email dan password pada penerima.
+        </div>
+    </div>
+    <div class="col-xs-0 col-md-2"></div>
+</div>
+<?php endif ?>
+
+
 <div class="row">
     <div class="col-lg-4 col-md-2"></div>
     <div class="col-lg-4 col-md-8 col-sm-12 text-center bg-light rounded p-4">
-        <form method="post" action="index.php?view=admin_daftarkan">
-            <div class="form-group">
-                <label for="user">Email:</label>
-                <input type="text" class="form-control" name="email">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h2>Daftarkan Administrator/Penindak Lanjut</h2>
             </div>
-            <div class="form-group">
-                <label for="pwd">Password:</label>
-                <input type="password" class="form-control" name="password">
+            <div class="panel-body">
+                <form method="post" action="index.php?view=admin_daftarkan">
+                    <div class="form-group">
+                        <label for="user">Email:</label>
+                        <input type="text" class="form-control" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd">Password:</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="btn-submit">Submit</button>
+                </form>
             </div>
-            <button type="submit" class="btn btn-primary" name="btn-submit">Submit</button>
-        </form>
+        </div>
     </div>
     <div class="col-lg-4 col-md-2"></div>
 </div>
-
-<?php
-
-if (isset($_POST['btn-submit']) && $_POST['email'] != '' && $_POST['password'] != ''){
-    $getParam = "&p=" . md5($_POST['password']) . "&e=" . md5($_POST['email']);
-    $link = "index.php?view=pegawai_regis{$getParam}";
-
-    echo $link;
-}
-
-?>
