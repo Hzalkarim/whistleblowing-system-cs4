0b119a58f89fffd5529dc05e33a2786b9d9ff507
hzalkarim <hafizh.alkarim02@gmail.com>
2020-05-03 06:35:17 +0700

    Added joinSelect and joinSelectOne method

--- class/controller/MahasiswaController.php
+++ class/controller/MahasiswaController.php
@@ -23,20 +23,53 @@ class MahasiswaController extends WbController {
 
     public function select(){
 
-        if (!is_null($this->getModel()))
-            $condition = $this->getModel()->getConditions();
-        else
-            $condition = 1;
+        $condition = !is_null($this->getModel()) ? $this->getModel()->getConditions() : 1;
 
         $mhs = new Mahasiswa();
         $col = implode(', ', $mhs->getColumns());
 
         $result = WbController::executeSelectQuery($col, 'mahasiswa', $condition);
-
         $arrResult = WbController::getArrayFromQueryResult($result, 'Mahasiswa');
 
-
         return $arrResult;
+    }
+
+    public function joinSelect() {
+        $condition = !is_null($this->getModel()) ? $this->getModel()->getConditions() : 1;
+
+        $mhs = new Mahasiswa();
+        $user = new User();
+        $prodi = new ProgramStudi();
+
+        $colMhs = array_map(
+            function($x) { return 'a.' . $x },
+            array_slice($mhs->getColumns(), 0, 1)
+        );
+        $colUser = array_map(function($x) { return 'b.' . $x }, $user->getColumns());
+        $colProdi = array_map(function($x) { return 'c.' . $x }, $user->getColumns());
+        $colArr = array_merge($colMhs, $colUser, $colProdi);
+        $col = implode(", ", $colArr);
+
+        $table = "(mahasiswa a LEFT JOIN user b ON a.user_id = b.id)
+                    LEFT JOIN program_studi c ON a.kode_prodi = c.kode";
+
+        $result = WbController::executeSelectQuery($col, $table, $condition);
+
+        $modelClassArr = Array(
+            "Mahasiswa" => count($colMhs),
+            "User" => count($colUser),
+            "ProgramStudi" => count($colProdi)
+        );
+
+        $resultArr = WbController::getArrayWithForeignModelFromQueryResult($result, $modelClassArr);
+
+        return $resultArr;
+    }
+
+    public function joinSelectOne() {
+        $result = $this->joinSelect();
+        if (!is_null($result) && count($result) > 0)
+            return $result[0];
     }
 
     public function getNimFromUserId($user_id) {
