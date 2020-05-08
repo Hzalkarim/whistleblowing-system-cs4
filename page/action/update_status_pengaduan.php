451d8dc0b02940d431e50aefe3cf406f90bba8b7
hzalkarim <hafizh.alkarim02@gmail.com>
2020-05-08 16:53:18 +0700

    Major cleaning and add notif

--- page/action/update_status_pengaduan.php
+++ page/action/update_status_pengaduan.php
@@ -1,31 +1,39 @@
 <?php
 
-	include "page/component/auth_validator/admin_validator.php";
+	include "../component/auth_validator/admin_validator.php";
 
-	require_once "class/controller/PengaduanController.php";
-    require_once "class/controller/PenindakLanjutController.php";
-    require_once "class/model/Pengaduan.php";
-    require_once "class/model/Mahasiswa.php";
-    require_once "class/model/Kategori.php";
-    require_once "class/model/PenindakLanjut.php";
-    require_once "class/model/User.php";
-    require_once "class/model/Penugasan.php";
+	require_once "../../class/wb_controller.php";
+	require_once "../../class/wb_model.php";
+	require_once "../../class/model/Pengaduan.php";
+    require_once "../../class/controller/PengaduanController.php";
 
-	if (isset($_GET['btn-final'])){
+	$result = '0';
+	if (isset($_GET['final_id_pgd'])){
 
     	$pengaduanCt = new PengaduanController();
-    	$c = "pengaduan.id = '".$_GET['id_pgd']."'";
+    	$c = "id = '".$_GET['final_id_pgd']."'";
     	$pengaduan = $pengaduanCt->where($c)->SelectOne();
     	$pengaduan->setStatus("Selesai");
-    	$pengaduan->update($pengaduan);
+    	$result = $pengaduanCt->update($pengaduan);
 	}
 
-	if (isset($_GET['btn-tunda'])){
+	if (isset($_GET['tunda_id_pgd'])){
 
     	$pengaduanCt = new PengaduanController();
-    	$c = "pengaduan.id = '".$_GET['id_pgd']."'";
+    	$c = "id = '".$_GET['tunda_id_pgd']."'";
     	$pengaduan = $pengaduanCt->where($c)->SelectOne();
     	$pengaduan->setStatus("Tertunda");
-    	$pengaduan->update($pengaduan);
-	}	
+    	$result = $pengaduanCt->update($pengaduan);
+	}
+
+	date_default_timezone_set("Asia/Bangkok");
+    if ($result == '1') {
+        setcookie('update-berhasil', 'hasil', time() + 2, "/");
+        setcookie('update-pesan', 'Status pengaduan berhasil diupdate', time() + 2, "/");
+    } else {
+        setcookie('update-gagal', 'gagal', time()+2, "/");
+        setcookie('update-pesan', 'Status pengaduan gagal diupdate', time() + 2, "/");
+    }
+
+	header("Location: ../../index.php?view=admin_pengaduan");
 ?>
