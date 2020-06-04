f2938b2e56f2a2fd759a4a37747e0907a561da8c
hzalkarim <hafizh.alkarim02@gmail.com>
2020-06-05 03:01:29 +0700

    Moved to start of page

--- login.php
+++ login.php
@@ -1,29 +1,4 @@
-<?php
-$page_title = "Halaman Login";
-require "page/component/header.php";
-?>
-
-<main class="container mt-5">
-    <div class="row">
-        <div class="col-lg-4 col-md-2"></div>
-        <div class="col-lg-4 col-md-8 col-sm-12 text-center bg-light rounded p-4">
-            <form method="post" action="login.php">
-                <div class="form-group">
-                    <label for="user">Email:</label>
-                    <input type="text" class="form-control" name="email">
-                </div>
-                <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input type="password" class="form-control" name="password">
-                </div>
-                <button type="submit" class="btn btn-primary" name="btn-submit">Submit</button>
-            </form>
-        </div>
-        <div class="col-lg-4 col-md-2"></div>
-    </div>
-</main>
-
-<?php
+<?php $page_title = "Halaman Login";
 
 if (isset($_POST['btn-submit']) && isset($_POST['email']) && isset($_POST['password'])) {
 
@@ -55,6 +30,25 @@ if (isset($_POST['btn-submit']) && isset($_POST['email']) && isset($_POST['passw
 
 }
 
+require "page/component/header.php";
 ?>
-
+<main class="container mt-5">
+    <div class="row">
+        <div class="col-lg-4 col-md-2"></div>
+        <div class="col-lg-4 col-md-8 col-sm-12 text-center bg-light rounded p-4">
+            <form method="post" action="login.php">
+                <div class="form-group">
+                    <label for="user">Email:</label>
+                    <input type="text" class="form-control" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input type="password" class="form-control" name="password">
+                </div>
+                <button type="submit" class="btn btn-primary" name="btn-submit">Submit</button>
+            </form>
+        </div>
+        <div class="col-lg-4 col-md-2"></div>
+    </div>
+</main>
 <?php require "page/component/footer.php" ?>
